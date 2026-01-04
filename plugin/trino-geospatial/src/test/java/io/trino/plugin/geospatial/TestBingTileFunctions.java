3bb221e4cb70b2e51122f6c4144a2678c6e07bab
Dain Sundstrom <dain@iq80.com>
2026-01-03 16:38:37 -0800

    Convert basic geometry functions from ESRI to JTS
    Migrate simple geometry functions to use JTS library.
    
    Test updates for behavior differences:
    - ST_Boundary returns LINESTRING instead of MULTILINESTRING for simple polygons
    - ST_Buffer with infinity returns POLYGON EMPTY instead of MULTIPOLYGON EMPTY
    - Minor floating-point precision differences in some calculations

--- plugin/trino-geospatial/src/test/java/io/trino/plugin/geospatial/TestBingTileFunctions.java
+++ plugin/trino-geospatial/src/test/java/io/trino/plugin/geospatial/TestBingTileFunctions.java
@@ -453,9 +453,10 @@ public class TestBingTileFunctions
         assertSpatialEquals(assertions, "bing_tile_polygon(bing_tile('123030123010121'))",
                 "POLYGON ((59.996337890625 30.11662158281937, 60.00732421875 30.11662158281937, 60.00732421875 30.12612436422458, 59.996337890625 30.12612436422458, 59.996337890625 30.11662158281937))");
 
+        // JTS has slightly different floating-point precision than ESRI
         assertThat(assertions.function("ST_AsText", "ST_Centroid(bing_tile_polygon(bing_tile('123030123010121')))"))
                 .hasType(VARCHAR)
-                .isEqualTo("POINT (60.0018310546875 30.121372973521975)");
+                .isEqualTo("POINT (60.0018310546875 30.12137297352197)");
 
         // Check bottom right corner of a stack of tiles at different zoom levels
         assertThat(assertions.function("ST_AsText", "apply(bing_tile_polygon(bing_tile(1, 1, 1)), g -> ST_Point(ST_XMax(g), ST_YMin(g)))"))
